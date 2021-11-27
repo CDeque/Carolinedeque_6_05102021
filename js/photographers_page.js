e9d7a7a192d69076fb2bf1e3381ecd640c0aba54
Dequé <carolinedeque@gmail.com>
2021-11-27 02:13:51 +0100

    CSS index & photographersPage

--- js/photographers_page.js
+++ js/photographers_page.js
@@ -30,15 +30,18 @@ class CreatePhotographerPage {
     this.createPhotographerCard();
     this.addingClass();
     this.addingTextContent();
+    this.createTags();
     this.linkElements();
   }
   // creation du HTML
   createPhotographerCard() {
     this.section = document.createElement("section");
+    this.article = document.createElement("article");
+    this.div = document.createElement("div");
     this.photographerName = document.createElement("h1");
     this.city = document.createElement("p");
     this.tagline = document.createElement("p");
-    this.tagsContainer = document.createElement("div");
+    this.tagsContainer = document.createElement("ul");
     this.contactBtn = document.createElement("button");
     this.photographerPortrait = document.createElement("img");
   }
@@ -46,12 +49,14 @@ class CreatePhotographerPage {
   //Ajout des classes
   addingClass() {
     this.section.classList.add("section");
-    this.photographerName.classList.add("name");
-    this.city.classList.add("location");
-    this.tagline.classList.add("tagline");
-    this.tagsContainer.classList.add("tags_container");
+    this.article.classList.add("photographer_profile");
+    this.div.classList.add("profile_container");
+    this.photographerName.classList.add("name_profile");
+    this.city.classList.add("location_profile");
+    this.tagline.classList.add("tagline_profile");
+    this.tagsContainer.classList.add("tags_container_profile");
     this.contactBtn.classList.add("contact_button");
-    this.photographerPortrait.classList.add("photo_id");
+    this.photographerPortrait.classList.add("photo_id_profile");
   }
 
   //ajout du contenu
@@ -65,16 +70,41 @@ class CreatePhotographerPage {
     this.photographerPortrait.alt = this.data.name;
     this.photographerPortrait.tabIndex = "0";
     this.photographerPortrait.id = this.data.id;
+    this.contactBtn.innerHTML = "Contactez-moi";
+  }
+
+  // Ajout des tags
+  createTags() {
+    this.tagAnchor = document.createElement("a");
+    this.tagAnchor.classList.add("tag_anchor");
+    this.tagAnchor.href = "#";
+
+    this.data.tags.forEach((tag) => {
+      const tagsArray = [];
+
+      this.tagList = document.createElement("li");
+      this.tagList.classList.add("profile_tag");
+      this.tagList.setAttribute("aria-label", tag);
+      this.tagList.innerHTML = "#" + tag;
+
+      this.tagAnchor.appendChild(this.tagList);
+
+      tagsArray.push(tag);
+      //console.log(tagsArray);
+    });
   }
 
   //Lier les éléments
   linkElements() {
     this.main.appendChild(this.section);
-    this.section.appendChild(this.photographerName);
-    this.section.appendChild(this.city);
-    this.section.appendChild(this.tagline);
-    this.section.appendChild(this.tagsContainer);
-    this.section.appendChild(this.contactBtn);
-    this.section.appendChild(this.photographerPortrait);
+    this.section.appendChild(this.article);
+    this.article.appendChild(this.div);
+    this.div.appendChild(this.photographerName);
+    this.div.appendChild(this.city);
+    this.div.appendChild(this.tagline);
+    this.div.appendChild(this.tagsContainer);
+    this.tagsContainer.appendChild(this.tagAnchor);
+    this.article.appendChild(this.contactBtn);
+    this.article.appendChild(this.photographerPortrait);
   }
 }
